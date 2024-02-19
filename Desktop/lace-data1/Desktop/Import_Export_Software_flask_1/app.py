493025c0697173d57dbb6ad1336ef4af5572c0c7
lancedsouza <lancelot.dsouza@gmail.com>
2024-02-19 16:53:00 +0530

    Delete Desktop/lace-data1/Desktop/Import_Export_Software_flask_1 directory

--- Desktop/lace-data1/Desktop/Import_Export_Software_flask_1/app.py
+++
@@ -1,9 +0,0 @@
-from Imports_1 import app
-from flask import render_template
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
